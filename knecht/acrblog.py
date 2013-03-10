1e28608e81f56e5ff37b2c586ad5d6a1e38b680a
Christoph Polcin <labs@polcin.de>
2013-03-10 10:33:41 +0100

    update log message

--- knecht/acrblog.py
+++ knecht/acrblog.py
@@ -11,14 +11,14 @@ from acrylamid.readers import load as loadentries
 class Engine():
     def __init__(self, session):
         try:
-            path = os.path.join(session.conf['blogbase'], session.user, session.conf.blogconf)
+            path = os.path.join(session.conf.blogbase, session.user, session.conf.blogconf)
             self.conf = loadconf(path)
             (self.__entries, self.__pages, _, self.__drafts) = loadentries(self.conf)
         except Exception as e:
-            log.critical("%s in `conf.py`" % e.__class__.__name__)
-            raise Exception("fail to initialize Acrylamid")
+            log.critical("fail to initialize Acrylamid: %s" % str(e))
+            raise e
         self.__user_drafts = [] # Draft in current user branche
-        
+
     def get_user_drafts(self):
         '''Get draft from unclosed user branche'''
         pass
