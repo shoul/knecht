678b799ae323df7500b091800fa67db3ad8d90ba
Christoph Polcin <labs@polcin.de>
2013-03-11 20:41:00 +0100

    update docs

--- knecht/engine/__init__.py
+++ knecht/engine/__init__.py
@@ -14,37 +14,47 @@ class Engine(object):
 
     @abc.abstractmethod
     def get_user_drafts(self):
-        '''Get unfinished user entries'''
+        '''Get unfinished user entries
+        :return: Generator over Entries or None'''
 
     @abc.abstractmethod
     def get_entries(self):
-        '''Get all the rest entries'''
+        '''Get all the rest entries
+        :return: Generator over Entries or None'''
 
     @abc.abstractmethod
     def get_pages(self):
-        '''Get static pages'''
+        '''Get static pages
+        :return: Generator over Entries or None'''
 
     @abc.abstractmethod
     def get_drafts(self):
-        '''Get all entries that marked as draft'''
+        '''Get all entries that marked as draft
+        :return: Generator over Entries or None'''
 
     @abc.abstractmethod
     def store_file(self, file_path, content):
         '''stores the content into a file
-           will do all the messy work'''
+        will do all the messy work
+        :raise IOError'''
 
     @abc.abstractmethod
     def finish_file(self, file_path):
         '''stores the content into a file
-           will do all the messy work'''
+         will do all the messy work
+        :raise IOError'''
 
     @abc.abstractmethod
     def get_file_content(self, file_path):
-        '''retrieves the entry content'''
+        '''retrieves the entry content
+        :raise IOError if file not found
+        :return: file content as str'''
 
     @abc.abstractmethod
     def preview(self, file_path=None):
-        '''renders a preview'''
+        '''renders a preview
+        :raise SyntaxWarning
+        :raise IOError'''
 
     @abc.abstractmethod
     def deploy(self):
