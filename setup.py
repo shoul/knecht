e02de01cecb1a20410a95638281dc9c91fbf7469
Christoph Polcin <labs@polcin.de>
2013-03-13 01:10:15 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,40 @@
 # -*- encoding: utf-8 -*-
 
-from distutils.core import setup
+from os.path import join, dirname
+from setuptools import setup, find_packages
 
 # reflect changes in "CHANGES.rst"
 version = '0.1'
 
 # package description
-desc = "Knecht is a micro Content Management System (CMS) for static blog generators"
+desc = "Knecht is a micro Content Management System (CMS) for static blog compiler"
 
-long_desc = []
-for _file in ('README.md', 'CHANGES.rst'):
-    with open(_file, 'r') as fh:
-        long_desc.append(fh.read())
+requires = ['Flask>=0.9']
 
 setup(
     name='knecht',
     version=version,
-    description=desc,
-    long_description='\n\n'.join(long_desc),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "License :: BSD 2 clauses",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
     author='Christoph Polcin <labs@polcin.de>, Frank Schneider <shoul@gmx.de>',
     author_email='dev@polcin.de',
     license='BSD',
+    url='https://git.christoph-polcin.com/knecht/',
+    description=desc,
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+       "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: BSD 2 clauses",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    install_requires=requires,
+    keywords='',
     py_modules=['knecht.__init__'],
 )
