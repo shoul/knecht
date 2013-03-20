71ae618a3e76f96b27dcc527e99535a387f3ce6f
Christoph Polcin <labs@polcin.de>
2013-03-20 20:06:44 +0100

    add setLevel to knecht.log

--- knecht/log.py
+++ knecht/log.py
@@ -1,29 +1,37 @@
 # -*- encoding: utf-8 -*-
 
 import logging
+from logging import INFO, DEBUG, WARN
 
+logger = None
 info = debug = warn = critical = error = fatal = exception = None
 i = d = w = c = e = f = x = None
 
-__all__ = ['info', 'debug', 'warn', 'critical', 'error', 'fatal', 'exception',
-            'i', 'd', 'w', 'c', 'e', 'f', 'x' ]
+__all__ = [ 'info', 'debug', 'warn', 'critical', 'error', 'fatal', 'exception',
+            'i', 'd', 'w', 'c', 'e', 'f', 'x',
+            'logger',
+            'INFO', 'DEBUG', 'WARN' ]
 
 def init(name, level=logging.WARN):
-    global info,  debug, warn, critical, error, fatal, exception, \
-            i, d, w, c, e, f, x
+    global info, debug, warn, critical, error, fatal, exception, \
+            i, d, w, c, e, f, x, \
+            logger
 
-    log = logging.getLogger(name)
+    logger = logging.getLogger(name)
     h = logging.StreamHandler()
     h.setLevel(level)
     fmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     h.setFormatter(fmt)
-    log.addHandler(h)
+    logger.addHandler(h)
 
-    i = info = log.info
-    d = debug = log.debug
-    w = warn = log.warn
-    c = critical = log.critical
-    e = error = log.error
-    f = fatal = log.fatal
-    x = exception = log.exception
+    i = info = logger.info
+    d = debug = logger.debug
+    w = warn = logger.warn
+    c = critical = logger.critical
+    e = error = logger.error
+    f = fatal = logger.fatal
+    x = exception = logger.exception
 
+def setLevel(level):
+    global logger
+    logger.setLevel(level)
